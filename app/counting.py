35ef03fc18de4775ea2deefdd9bf7d02a3aefcb8
LemanSash <leman.sash@ya.ru>
2025-06-15 18:36:12 +0300

    Removed some comments

--- app/counting.py
+++ app/counting.py
@@ -17,26 +17,7 @@ def load_inflation_data():
     return inflation
 
 def calculate_real_salaries(salaries_melted, inflation, base_year=2024):
-    # inflation_dict = df_inflation.set_index("Year")["Inflation"].to_dict()
-    # inflation_multipliers = {}
-
-    # cumulative = 1.0
-    # for year in sorted(inflation_dict.keys(), reverse=True):
-    #     if year == base_year:
-    #         inflation_multipliers[year] = 1.0
-    #     else:
-    #         cumulative *= 1 + inflation_dict[year] / 100
-    #         inflation_multipliers[year] = cumulative
-
-    # df_salaries["InflationFactor"] = df_salaries["Year"].map(lambda y: inflation_multipliers.get(y, 1.0))
-    # df_salaries["RealSalary"] = df_salaries["Salary"] / df_salaries["InflationFactor"]
-
-    # df_salaries.sort_values(["Sector", "Year"], inplace=True)
-    # df_salaries["RealSalaryChange%"] = df_salaries.groupby("Sector")["RealSalary"].pct_change() * 100
-
     years = sorted(inflation.index.unique())
-
-    # Множители инфляции для каждого года до base_year
     inflation_multipliers = {
         year: np.prod([1 + inflation.loc[y, "Всего"]/100 for y in range(year+1, base_year+1) if y in inflation.index])
         for year in years
@@ -45,7 +26,6 @@ def calculate_real_salaries(salaries_melted, inflation, base_year=2024):
     salaries_melted["InflationMultiplier"] = salaries_melted["Year"].map(inflation_multipliers)
     salaries_melted["RealSalary"] = salaries_melted["Salary"] / salaries_melted["InflationMultiplier"]
 
-    # === РАСЧЁТ РАЗНИЦЫ ПО ГОДАМ ===
     salaries_melted = salaries_melted.sort_values(["Sector", "Year"])
     salaries_melted["RealSalaryChange%"] = salaries_melted.groupby("Sector")["RealSalary"].pct_change() * 100
 
